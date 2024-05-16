ae31c200a8cb4983b555db1548a7f2e0ff8b78db
Dreamland <3527551187@qq.com>
2024-05-16 09:46:38 +0800

    用于测试关于文档增加

--- docs-source/src/.vuepress/config.ts
+++ docs-source/src/.vuepress/config.ts
@@ -21,6 +21,15 @@ export default {
         sidebar: sideBarItems,
         sidebarDepth: 2,
         locales: {
+            '/kotlin/en/': {
+                navbar: navBarItems['/kotlin/en/'],
+                selectLanguageText: 'English (US)',
+                selectLanguageName: 'English',
+                editLinkText: 'Edit this page on GitHub',
+                tip: 'Tips',
+                warning: 'Notice',
+                danger: 'Pay Attention',
+            },
             '/en/': {
                 navbar: navBarItems['/en/'],
                 selectLanguageText: 'English (US)',
@@ -29,6 +38,21 @@ export default {
                 tip: 'Tips',
                 warning: 'Notice',
                 danger: 'Pay Attention',
+            },
+            '/kotlin/zh-cn/': {
+                navbar: navBarItems['/kotlin/zh-cn/'],
+                selectLanguageText: '简体中文 (CN)',
+                selectLanguageName: '简体中文',
+                editLinkText: '在 GitHub 上编辑此页',
+                notFound: ['这里什么都没有', '我们怎么到这来了？', '这是一个 404 页面', '看起来我们进入了错误的链接'],
+                backToHome: '回到首页',
+                contributorsText: '贡献者',
+                lastUpdatedText: '上次更新',
+                tip: '小提示',
+                warning: '注意',
+                danger: '特别注意',
+                openInNewWindow: '在新窗口中打开',
+                toggleColorMode: '切换颜色模式'
             },
             '/zh-cn/': {
                 navbar: navBarItems['/zh-cn/'],
@@ -52,7 +76,9 @@ export default {
         searchPlugin({
             isSearchable: (page) => page.path !== '/',
             locales: {
+                '/kotlin/en/': { placeholder: 'Search' },
                 '/en/': { placeholder: 'Search' },
+                '/kotlin/zh-cn/': { placeholder: '搜索' },
                 '/zh-cn/': { placeholder: '搜索' }
             }
         })
--- docs-source/src/.vuepress/configs/template.ts
+++ docs-source/src/.vuepress/configs/template.ts
@@ -62,9 +62,19 @@ export const configs = {
         logo: '/images/logo.png',
         title: 'Yuki Reflection',
         locales: {
+            '/kotlin/en/': {
+                title: 'KYuki Reflection',
+                lang: 'en-US',
+                description: 'An efficient Reflection API for Java and Android built in Kotlin.Fully attach Kotlin high-order reflections.'
+            },
             '/en/': {
                 lang: 'en-US',
                 description: 'An efficient Reflection API for Java and Android built in Kotlin'
+            },
+            '/kotlin/zh-cn/': {
+                title: 'KYuki Reflection',
+                lang: 'zh-CN',
+                description: '一个使用 Kotlin 构建的用于 Java 和 Android 平台高效反射 API - 完全附加 Kotlin 高阶反射'
             },
             '/zh-cn/': {
                 lang: 'zh-CN',
@@ -80,6 +90,43 @@ export const configs = {
 };
 
 export const navBarItems = {
+    '/kotlin/en/': [{
+        text: 'Navigation',
+        children: [{
+            text: 'Get Started',
+            children: [
+                { text: 'Introduce', link: i18n.string(navigationLinks.start[0], 'en') },
+                { text: 'Quick Start', link: i18n.string(navigationLinks.start[1], 'en') }
+            ]
+        }, {
+            text: 'Configs',
+            children: [
+                { text: 'API Basic Configs', link: i18n.string(navigationLinks.config[0], 'en') },
+                { text: 'API Exception Handling', link: i18n.string(navigationLinks.config[1], 'en') }
+            ]
+        }, {
+            text: 'API Document',
+            children: [{ text: 'Document Introduction', link: i18n.string(navigationLinks.apiDocs[0], 'en') }, {
+                text: 'Public API',
+                link: i18n.string(navigationLinks.publicApi[0], 'en'),
+                activeMatch: i18n.string(navigationLinks.apiDocs[1], 'en')
+            }, {
+                text: 'Features',
+                link: i18n.string(navigationLinks.apiDocs[2], 'en')
+            }]
+        }, {
+            text: 'About',
+            children: [
+                { text: 'Changelog', link: i18n.string(navigationLinks.about[0], 'en') },
+                { text: 'Looking for Future', link: i18n.string(navigationLinks.about[1], 'en') },
+                { text: 'Contact Us', link: i18n.string(navigationLinks.about[2], 'en') },
+                { text: 'About this Document', link: i18n.string(navigationLinks.about[3], 'en') }
+            ]
+        }]
+    }, {
+        text: 'Contact Us',
+        link: i18n.string(navigationLinks.about[2], 'en')
+    }],
     '/en/': [{
         text: 'Navigation',
         children: [{
@@ -116,6 +163,43 @@ export const navBarItems = {
     }, {
         text: 'Contact Us',
         link: i18n.string(navigationLinks.about[2], 'en')
+    }],
+    '/kotlin/zh-cn/': [{
+        text: '导航',
+        children: [{
+            text: '入门',
+            children: [
+                { text: '介绍', link: i18n.string(navigationLinks.start[0], 'zh-cn') },
+                { text: '快速开始', link: i18n.string(navigationLinks.start[1], 'zh-cn') }
+            ]
+        }, {
+            text: '配置',
+            children: [
+                { text: 'API 基本配置', link: i18n.string(navigationLinks.config[0], 'zh-cn') },
+                { text: 'API 异常处理', link: i18n.string(navigationLinks.config[1], 'zh-cn') }
+            ]
+        }, {
+            text: 'API 文档',
+            children: [{ text: '文档介绍', link: i18n.string(navigationLinks.apiDocs[0], 'zh-cn') }, {
+                text: 'Public API',
+                link: i18n.string(navigationLinks.publicApi[0], 'zh-cn'),
+                activeMatch: i18n.string(navigationLinks.apiDocs[1], 'zh-cn')
+            }, {
+                text: '功能介绍',
+                link: i18n.string(navigationLinks.apiDocs[2], 'zh-cn')
+            }]
+        }, {
+            text: '关于',
+            children: [
+                { text: '更新日志', link: i18n.string(navigationLinks.about[0], 'zh-cn') },
+                { text: '展望未来', link: i18n.string(navigationLinks.about[1], 'zh-cn') },
+                { text: '联系我们', link: i18n.string(navigationLinks.about[2], 'zh-cn') },
+                { text: '关于此文档', link: i18n.string(navigationLinks.about[3], 'zh-cn') }
+            ]
+        }]
+    }, {
+        text: '联系我们',
+        link: i18n.string(navigationLinks.about[2], 'zh-cn')
     }],
     '/zh-cn/': [{
         text: '导航',
@@ -157,6 +241,27 @@ export const navBarItems = {
 };
 
 export const sideBarItems = {
+    '/kotlin/en/': [{
+        text: 'Get Started',
+        collapsible: true,
+        children: i18n.array(navigationLinks.start, 'en')
+    }, {
+        text: 'Configs',
+        collapsible: true,
+        children: i18n.array(navigationLinks.config, 'en')
+    }, {
+        text: 'API Document',
+        collapsible: true,
+        children: [i18n.string(navigationLinks.apiDocs[0], 'en'), {
+            text: 'Public API' + i18n.space,
+            collapsible: true,
+            children: i18n.array(navigationLinks.publicApi, 'en')
+        }, i18n.string(navigationLinks.apiDocs[2], 'en')]
+    }, {
+        text: 'About',
+        collapsible: true,
+        children: i18n.array(navigationLinks.about, 'en')
+    }],
     '/en/': [{
         text: 'Get Started',
         collapsible: true,
@@ -177,6 +282,27 @@ export const sideBarItems = {
         text: 'About',
         collapsible: true,
         children: i18n.array(navigationLinks.about, 'en')
+    }],
+    '/kotlin/zh-cn/': [{
+        text: '入门',
+        collapsible: true,
+        children: i18n.array(navigationLinks.start, 'zh-cn')
+    }, {
+        text: '配置',
+        collapsible: true,
+        children: i18n.array(navigationLinks.config, 'zh-cn')
+    }, {
+        text: 'API 文档',
+        collapsible: true,
+        children: [i18n.string(navigationLinks.apiDocs[0], 'zh-cn'), {
+            text: 'Public API' + i18n.space,
+            collapsible: true,
+            children: i18n.array(navigationLinks.publicApi, 'zh-cn')
+        }, i18n.string(navigationLinks.apiDocs[2], 'zh-cn')]
+    }, {
+        text: '关于',
+        collapsible: true,
+        children: i18n.array(navigationLinks.about, 'zh-cn')
     }],
     '/zh-cn/': [{
         text: '入门',
